8dba8f0fff9201da34f757ae6348741ae35944f0
plaxi0s <pawan.dogra99@gmail.com>
2022-01-12 20:59:10 +0530

    split existing benches in benches/trie_benches.rs

--- benches/trie_benches.rs
+++ benches/trie_benches.rs
@@ -1,12 +1,11 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use radix_trie::Trie;
 
-fn get_text() -> Vec<String> {
+fn get_text(file: &str) -> Vec<String> {
     use std::fs::File;
     use std::io::Read;
-    const DATA: &[&str] = &["data/1984.txt", "data/sun-rising.txt"];
     let mut contents = String::new();
-    File::open(&DATA[1])
+    File::open(file)
         .unwrap()
         .read_to_string(&mut contents)
         .unwrap();
@@ -16,6 +15,7 @@ fn get_text() -> Vec<String> {
         .collect()
 }
 
+#[inline]
 fn make_trie(words: &[String]) -> Trie<&str, usize> {
     let mut trie = Trie::new();
     for w in words {
@@ -24,37 +24,70 @@ fn make_trie(words: &[String]) -> Trie<&str, usize> {
     trie
 }
 
-fn trie_insert(b: &mut Criterion) {
-    let words = get_text();
-    b.bench_function("trie insert", |b| b.iter(|| make_trie(&words)));
+macro_rules! bench_insert {
+    ($name: ident, $dataset: literal,$func: ident) => {
+        fn $name(b: &mut Criterion) {
+            let words = get_text($dataset);
+            b.bench_function(stringify!($name), |b| b.iter(|| $func(&words)));
+        }
+    };
 }
 
-fn trie_get(b: &mut Criterion) {
-    let words = get_text();
-    let trie = make_trie(&words);
-    b.bench_function("trie get", |b| {
-        b.iter(|| {
-            words
-                .iter()
-                .map(|w| trie.get(&&w[..]))
-                .collect::<Vec<Option<&usize>>>()
-        })
-    });
+macro_rules! bench_get {
+    ($name: ident, $dataset: literal,$func: ident) => {
+        fn $name(b: &mut Criterion) {
+            let words = get_text($dataset);
+            let trie = $func(&words);
+
+            b.bench_function(stringify!($name), |b| {
+                b.iter(|| {
+                    words
+                        .iter()
+                        .map(|w| trie.get(&&w[..]))
+                        .collect::<Vec<Option<&usize>>>()
+                })
+            });
+        }
+    };
 }
 
-fn trie_insert_remove(b: &mut Criterion) {
-    let words = get_text();
-
-    b.bench_function("trie remove", |b| {
-        b.iter(|| {
-            let mut trie = make_trie(&words);
-            for w in &words {
-                trie.remove(&&w[..]);
-            }
-        });
-    });
+macro_rules! bench_remove {
+    ($name: ident, $dataset: literal,$func: ident) => {
+        fn $name(b: &mut Criterion) {
+            let words = get_text($dataset);
+            b.bench_function(stringify!($name), |b| {
+                b.iter(|| {
+                    let mut trie = $func(&words);
+                    for w in &words {
+                        trie.remove(&&w[..]);
+                    }
+                })
+            });
+        }
+    };
 }
 
-criterion_group!(benches, trie_insert, trie_get, trie_insert_remove);
+bench_insert!(trie_insert_1984, "data/1984.txt", make_trie);
+bench_insert!(trie_insert_sun_rising, "data/sun-rising.txt", make_trie);
 
-criterion_main!(benches);
+bench_insert!(trie_get_1984, "data/1984.txt", make_trie);
+bench_get!(trie_get_sun_rising, "data/sun-rising.txt", make_trie);
+
+bench_remove!(trie_remove_1984, "data/1984.txt", make_trie);
+bench_remove!(trie_remove_sun_rising, "data/sun-rising.txt", make_trie);
+
+criterion_group!(
+    benches_sun_rising,
+    trie_insert_sun_rising,
+    trie_get_sun_rising,
+    trie_remove_sun_rising,
+);
+
+criterion_group!(
+    benches_1984,
+    trie_insert_1984,
+    trie_get_1984,
+    trie_remove_1984,
+);
+
+criterion_main!(benches_sun_rising, benches_1984);
